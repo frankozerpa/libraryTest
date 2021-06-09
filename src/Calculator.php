7c025a7e5ed6bfecab1023cfc77c64bb9ddeb004
FranKo <franko.zerpa@tata.com.uy>
2021-06-09 13:50:42 -0300

    calculator library

---
+++ src/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Proyecto\Math;
+
+class Calculator
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itCanTestSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
